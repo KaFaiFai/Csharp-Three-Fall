32e16f03d04b49c82b86256b189e7115ca5f7542
Cyrus <m19990328@gmail.com>
2024-06-01 00:45:13 +0800

    + connect to custom signals, async program with tween signal

--- scenes/Block.cs
+++ scenes/Block.cs
@@ -3,38 +3,45 @@ using System;
 
 public partial class Block : Node
 {
-	public BlockType Type { get; set; } = BlockType.d;
+    public BlockType Type { get; set; } = BlockType.d;
 
-	// Called when the node enters the scene tree for the first time.
-	public override void _Ready()
-	{
-		ColorRect colorRect = GetNode<ColorRect>("ColorRect");
-		colorRect.Color = Type switch
-		{
-			BlockType.a => Colors.Blue,
-			BlockType.b => Colors.Violet,
-			BlockType.c => Colors.Yellow,
-			BlockType.d => Colors.Green,
-			_ => Colors.Black,
-		};
+    // Called when the node enters the scene tree for the first time.
+    public override void _Ready()
+    {
+        ColorRect colorRect = GetNode<ColorRect>("ColorRect");
+        colorRect.Color = Type switch
+        {
+            BlockType.a => Colors.Blue,
+            BlockType.b => Colors.Violet,
+            BlockType.c => Colors.Yellow,
+            BlockType.d => Colors.Green,
+            _ => Colors.Black,
+        };
+    }
 
-		InputEvents inputEvents = GetNode<InputEvents>("InputEvents");
-		GD.Print(inputEvents.GetSignalList());
-		inputEvents.MovedLeft += OnMovedLeft;
-		inputEvents.MovedRight += OnMovedRight;
-	}
+    // Called every frame. 'delta' is the elapsed time since the previous frame.
+    public override void _Process(double delta)
+    {
+    }
 
-	// Called every frame. 'delta' is the elapsed time since the previous frame.
-	public override void _Process(double delta)
-	{
-	}
-
-	private void OnMovedLeft()
-	{
-		GD.Print("OnMovedLeft");
-	}
-	private void OnMovedRight()
-	{
-		GD.Print("OnMovedRight");
-	}
+    private async void _on_input_events_moved_left()
+    {
+        GD.Print("Moved Left");
+        Tween tween = CreateTween();
+        tween.TweenProperty(this, "position", new Vector2(300, 300), 1f);
+        await ToSignal(tween, "finished");
+        GD.Print("Moved Left Finished");
+        tween = CreateTween();
+        tween.TweenProperty(this, "position", new Vector2(200, 300), 1f);
+    }
+    private async void _on_input_events_moved_right()
+    {
+        GD.Print("Moved Right");
+        Tween tween = CreateTween();
+        tween.TweenProperty(this, "position", new Vector2(700, 300), 1f);
+        await ToSignal(tween, "finished");
+        GD.Print("Moved Right Finished");
+        tween = CreateTween();
+        tween.TweenProperty(this, "position", new Vector2(800, 300), 1f);
+    }
 }
